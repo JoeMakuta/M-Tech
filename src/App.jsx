810d853665883afd90e506402463cb64e8f70ca3
Josué Makuta <makutajosue@gmail.com>
2022-12-12 14:21:55 +0100

    Initial commit : Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+  return (
+    <div className=" h-[100vh] font-bold justify-center items-center flex">
+      <h1 className=" text-2xl " >M-TECH SITE HERE ...</h1>
+    </div>
+  )
+}
+
+export default App
